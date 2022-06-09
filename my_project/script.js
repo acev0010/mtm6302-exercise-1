b2a48eca06307403cd7cc854adfa94af76390b10
Isabel Acevedo <acev0010@algonquinlive.com>
2022-06-08 21:30:47 -0400

    attempt_one

---
+++ my_project/script.js
@@ -0,0 +1,9 @@
+// The following line demonstrates how to log a message to the console
+console.log('Hello, World!')
+
+// The following line demonstrates how to log a warning to the console
+console.warn('Take me to your leader!')
+
+let example = 'Dylan';
+example = 'Israel';
+console.log(example);
