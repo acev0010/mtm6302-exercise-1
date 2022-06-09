3ac33597993aa34e35468f7a90045919f93addb7
Isabel Acevedo <acev0010@algonquinlive.com>
2022-06-09 15:09:10 -0400

    versionpedro

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // back tick `
 
+// let emojis = [129408, 129409, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
+//     0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F4DA,
+//     0x1F431, 0x1F42A, 0x1F439, 0x1F424];
 
-const p = document.createElement("p");
+
+// const p = document.createElement("p");
+// const collection = document.getElementsByClassName("block");
+// collection[2].innerHTML = "&#129409;";
+
+
 // const node = document.createTextNode("This is new.");
 // p.appendChild(node);
 // const element = document.getElementById("blocks");
 // element.appendChild(p);
-// const $emoji = document.getElementById('blocks')
-
 
 // p.innerText = "This is a paragraph";
 // document.body.appendChild(p);
@@ -16,30 +22,46 @@ const p = document.createElement("p");
 
 // const p = document.getElementById('blocks');
 
-const emoji = []
+function loadEmojis ()
+
+{
 
 
-for (let j = 129408; j <= 129458; j++) {
-    let emojicode = (`&#${j};`);
-    emoji.push(`<div class="blocks">${j}</div>`)
+const emojiSection = document.createElement('div');
+const emojis = document.createElement('p');
+
+let emojiCount = 50
+
+for (let j = 129409; j <= 129459; j++) {
+    let emojiCode = `&#${j};`
+    emojis.innerHTML += emojiCode
+}
+    
+    
+    // console.log(`&#${j};`);
+    // emoji.push(`<div class="block"><span style="font-size: 5rem;"> (emojicode) </span><br></div>`)
 
     // for(let i = 1; i <= j; i++) {
     //     console.log(`&#${i};`);
+    //     emoji.push(`<div class="blocks"></div>`)
     // }
 
-    emoji.push(`</div>`)
-}
+    // emoji.push(`</div>`)
+    // console.log(emojicode)
 
-console.log(emoji)
+    emojiSection.appendChild(emojis)
+    document.body.appendChild(emojiSection)
+
+}
 
 emoji.innerHTML += emoji.join('')
 
 console.log(emoji.innerHTML)
 
 
-// if (typeof $main !== 'undefined' && typeof $main != null && typeof blocks === 'object') {
-//     $main.innerHTML = blocks.join('')
-// }
+if (typeof $main !== 'undefined' && typeof $main != null && typeof blocks === 'object') {
+    $main.innerHTML = blocks.join('')
+}
 
 // const $emoji = document.createElement("p")
 // $emoji.innerHTML = emoji.join('')
